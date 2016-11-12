238531eb615cb7260425bfe8ef3197f08b0410c5
Sofian Brabez <sbz@FreeBSD.org>
2016-11-12 08:14:55 +0100

    Alpha reorder of the headers

--- ctfdump.c
+++ ctfdump.c
@@ -14,16 +14,16 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#include <sys/param.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <sys/mman.h>
+#include <sys/param.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #include <err.h>
 #include <fcntl.h>
 #include <locale.h>
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
